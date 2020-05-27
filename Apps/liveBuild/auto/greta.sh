28badae8f53b59ad434bd895490687a15d973e68
Leandro PerronaPC <oroverdino@gmail.com>
2020-05-27 10:54:12 -0300

    update liveBuild

---
+++ Apps/liveBuild/auto/greta.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+lb config noauto \
+    --mode debian \
+    --interactive shell \
+    --distribution buster \
+    --architectures amd64 \
+    --debian-installer false \
+    --updates true \
+    --archive-areas "main contrib non-free" \
+    --apt aptitude \
+    --apt-indices false \
+    --memtest none \
+    --bootappend-live "boot=live components keyboard-layouts=us keyboard-variants=dvorak persistence persistence-encryption=luks"
+    "${@}"
