a1816a3681c841de28cca025608cc53e33356d53
Leandro PerronaPC <oroverdino@gmail.com>
2020-05-28 00:14:13 -0300

    update liveBuild, add firmware list

--- Apps/liveBuild/greta.sh
+++ Apps/liveBuild/greta.sh
@@ -1,17 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
-set -e
-
-lb config noauto \
+lb config \
     --mode debian \
-    --interactive shell \
+    --binary-images iso-hybrid \
+    --iso-application Greta \
+    --iso-volume Greta \
     --distribution buster \
     --architectures amd64 \
-    --debian-installer false \
+    --debian-installer live \
+    --debian-installer-gui false \
     --updates true \
     --archive-areas "main contrib non-free" \
     --apt aptitude \
     --apt-indices false \
+    --apt-recommends true \
     --memtest none \
-    --bootappend-live "boot=live components keyboard-layouts=us keyboard-variants=dvorak persistence persistence-encryption=luks"
+    --interactive shell \
+    --bootappend-live "boot=live components keyboard-layouts=es,us keyboard-variants=,dvorak persistence persistence-media=removable-usb"
     "${@}"
