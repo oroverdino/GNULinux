848daefd5588f94b6c28917ce60b867f6dd3ba76
Leandro Torres <oroverdino@gmail.com>
2020-07-25 19:52:49 -0300

    update i3 config

--- scripts/gugler.sh
+++ scripts/gugler.sh
@@ -3,20 +3,17 @@
 case "$1" in
     push)
         echo "push to drive - - - - - - - - - - - - - - - - - - - - - - -"
-        rclone -P sync /home/leandro/Documents/Gugler drive:/Gugler
+        rclone -P sync /home/leandro/Documents/Gugler drive:Gugler
         ;;
     pull)
         echo "pull from drive - - - - - - - - - - - - - - - - - - - - - - -"
-        rclone -P sync drive:/Gugler /home/leandro/Documents/Gugler
+        rclone -P sync drive:Gugler /home/leandro/Documents/Gugler
         ;;
-    dry)
-        echo "first to - - - - - - - - - - - - - - - - - - - - - - -"
-        rclone sync --dry-run /home/leandro/Documents/Gugler drive:/Gugler
-        echo ""
-        echo "then from - - - - - - - - - - - - - - - - - - - - - - -"
-        rclone sync --dry-run drive:/Gugler /home/leandro/Documents/Gugler
+    check)
+        echo "check  - - - - - - - - - - - - - - - - - - - - - - - -"
+        rclone sync -P check /home/leandro/Documents/Gugler drive:Gugler
         ;;
     *)
-        echo "push or pull, but first do a dry"
+        echo "push or pull, but first check"
         ;;
 esac
